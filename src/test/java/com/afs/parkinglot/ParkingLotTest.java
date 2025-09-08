a9458c45608fa1a84fb925269cd488cc02c10d1b
SULE <sule1@oocl.com>
2025-09-08 16:57:47 +0800

    test: add should_return_the_right_car_with_each_ticket_when_Given_a_parking_lot_with_two_car_and_two_parking_tickets unitTest

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -24,6 +24,19 @@ public class ParkingLotTest {
         Car car2 =parkingLot.fetch(ticketResult);
         assertEquals(car,car2);
     }
+    //Case 3-Given a parking lot with two parked cars,and two parking tickets,When fetch the car twice,Then return the right car with each ticket
+    @Test
+    public void should_return_the_right_car_with_each_ticket_when_Given_a_parking_lot_with_two_car_and_two_parking_tickets() {
+        ParkingLot parkingLot = new ParkingLot();
+        Car car1 = new Car("1");
+        Car car2 = new Car("2");
+        Ticket ticketResult1 = parkingLot.park(car1);
+        Ticket ticketResult2 = parkingLot.park(car2);
 
+        Car car1Result = parkingLot.fetch(ticketResult1);
+        Car car2Result = parkingLot.fetch(ticketResult2);
+        assertEquals(car1,car1Result);
+        assertEquals(car2,car2Result);
+    }
 
 }
