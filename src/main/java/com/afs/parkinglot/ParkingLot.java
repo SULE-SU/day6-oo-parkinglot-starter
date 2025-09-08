b126a7fe7a36ff7c213b30a1913f35fe5ef95e7c
SULE <sule1@oocl.com>
2025-09-08 19:58:33 +0800

    feat: init ParkingLot

--- src/main/java/com/afs/parkinglot/ParkingLot.java
+++ src/main/java/com/afs/parkinglot/ParkingLot.java
@@ -1,4 +1,20 @@
 package com.afs.parkinglot;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 public class ParkingLot {
+    private Integer capacity;
+    private static final Integer CAPACITY = 10;
+    private Map<Ticket, Car> ticketCars = new HashMap<>();
+
+    public ParkingLot() {
+        this.capacity = CAPACITY;
+    }
+
+    public ParkingLot(Integer capacity) {
+        this.capacity = capacity;
+    }
+
 }
