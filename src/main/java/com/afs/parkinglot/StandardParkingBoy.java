7fc5aec9afd6c08c521b9bd55806cb9ed83aa667
SULE <sule1@oocl.com>
2025-09-08 20:47:05 +0800

    feat(story3): add and init StandardParkingBoy class

---
+++ src/main/java/com/afs/parkinglot/StandardParkingBoy.java
@@ -0,0 +1,11 @@
+package com.afs.parkinglot;
+
+public class StandardParkingBoy {
+    private ParkingLot parkingLot;
+
+    public StandardParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+
+}
