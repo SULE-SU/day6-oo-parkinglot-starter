2b5e6f5c4aed7f01d9cb84cb17efd00fd1f5caa6
SULE <sule1@oocl.com>
2025-09-08 16:55:53 +0800

    test: add should_return_a_parking_ticket_when_Given_a_parking_lot_and_a_car unitTest

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -1,4 +1,19 @@
 package com.afs.parkinglot;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ParkingLotTest {
+    //Case 1-Given a parking lot,and a car,When park the car,Then return a parking ticket.
+    @Test
+    public void should_return_a_parking_ticket_when_Given_a_parking_lot_and_a_car() {
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car("1");
+        Ticket ticket = new Ticket(car,1,parkingLot);
+        Ticket ticketResult = parkingLot.park(car);
+        assertEquals(ticket,ticketResult);
+    }
+
+
 }
